63a2ad8db0e730a271fbfde8d5a448cc7d078db5
Premnaath <premnaathvaradharajan@gmail.com>
2024-11-15 08:23:22 +0530

    add treesitter

---
+++ nvim/.config/nvim/lua/sudo-prem/plugins/leap.lua
@@ -0,0 +1,9 @@
+return {
+  "ggandor/leap.nvim",
+  config = function()
+    vim.keymap.set('n',        'f', '<Plug>(leap)')
+    vim.keymap.set('n',        'F', '<Plug>(leap-from-window)')
+    vim.keymap.set({'x', 'o'}, 'f', '<Plug>(leap-forward)')
+    vim.keymap.set({'x', 'o'}, 'F', '<Plug>(leap-backward)')
+  end,
+}
--- nvim/.config/nvim/lua/sudo-prem/plugins/telescope.lua
+++ nvim/.config/nvim/lua/sudo-prem/plugins/telescope.lua
@@ -1,15 +1,19 @@
-local M = {
-  'nvim-telescope/telescope.nvim',
-branch = '0.1.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
+return {
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
   },
-  config = function()
-    local telescope = require('telescope')
-    local actions = require('telescope.actions')
-local builtin = require('telescope.builtin')
-    require('telescope').setup {
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = '0.1.x',
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+        local builtin = require("telescope.builtin")
+        local actions = require('telescope.actions')
+        require("telescope").load_extension("ui-select")
+      require("telescope").setup{
       defaults = {
         mappings = {
           i = {
@@ -29,7 +33,7 @@ local builtin = require('telescope.builtin')
         },
         buffers = {
           initial_mode = 'normal',
-          sort_lastused = true,
+          sort_last_used = true,
         },
       },
       live_grep = {
@@ -48,20 +52,11 @@ local builtin = require('telescope.builtin')
       },
     }
 
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = 'Telescope find marks' })
-    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Telescope find word' })
-    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
-    vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = 'Telescope dot files' })
-    vim.keymap.set('n', '<leader>fds', function()
-      builtin.lsp_document_symbols {
-        symbols = { 'Class', 'Function', 'Method', 'Constructor', 'Interface', 'Module', 'Property' },
-      }
-    end, { desc = 'Telescope LSP document symbols' })
-  end,
-}
 
-return M
+      vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+        end,
+    },
+}
