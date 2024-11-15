c6ee028676a65d67c6f2c71a847c16d7beb8b461
Premnaath <premnaathvaradharajan@gmail.com>
2024-11-15 05:38:54 +0530

    update neo-tree

--- nvim/.config/nvim/lua/sudo-prem/init.lua
+++ nvim/.config/nvim/lua/sudo-prem/init.lua
@@ -1,5 +1,4 @@
 require("sudo-prem.core.options")
 require("sudo-prem.core.keymaps")
-require("sudo-prem.core.icons")
 require("sudo-prem.core.lazy")
 require("sudo-prem.core.commands")
--- nvim/.config/nvim/lua/sudo-prem/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/sudo-prem/plugins/neo-tree.lua
@@ -1,22 +1,98 @@
-local M = {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-        filesystem = {
-            filtered_items = {
-                visible = true,
-            },
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  event = 'VeryLazy',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {
+      popup_border_style = 'rounded',
+      window = {
+        position = 'left',
+        width = 35,
+        mapping_options = {
+          noremap = true,
+          nowait = true,
         },
-    },
-    config = function(_, opts)
-        require("neo-tree").setup(opts)
-        vim.cmd [[nnoremap \ :Neotree toggle<CR>]]
-    end
+        mappings = {
+          ['<esc>'] = 'cancel',
+          ['P'] = { 'toggle_preview', config = { use_float = false } },
+          ['l'] = 'open',
+          ['h'] = 'close_node',
+          ['<C-h>'] = 'close_all_subnodes',
+          ['t'] = 'open_tabnew',
+          ['y'] = 'copy_to_clipboard',
+          ['x'] = 'cut_to_clipboard',
+          ['p'] = 'paste_from_clipboard',
+          ["c"] = {
+            "copy",
+            config = {
+              show_path = "absolute"
+            }
+          },
+          ['m'] = 'move',
+          ['q'] = 'close_window',
+          ['R'] = 'refresh',
+          ['?'] = 'show_help',
+          ['<'] = 'prev_source',
+          ['>'] = 'next_source',
+          ['i'] = 'show_file_details',
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+        follow_current_file = {
+          enabled = false,
+          leave_dirs_open = false,
+        },
+        group_empty_dirs = false,
+        hijack_netrw_behavior = 'open_default',
+        use_libuv_file_watcher = false,
+        window = {
+          mappings = {
+            ['<bs>'] = 'navigate_up',
+            ['.'] = 'set_root',
+            ['H'] = 'toggle_hidden',
+            ['/'] = 'fuzzy_finder',
+            ['D'] = 'fuzzy_finder_directory',
+            ['f'] = 'filter_on_submit',
+            ['<c-x>'] = 'clear_filter',
+            ['[g'] = 'prev_git_modified',
+            [']g'] = 'next_git_modified',
+          },
+          fuzzy_finder_mappings = {
+            ['<down>'] = 'move_cursor_down',
+            ['<C-j>'] = 'move_cursor_down',
+            ['<up>'] = 'move_cursor_up',
+            ['<C-k>'] = 'move_cursor_up',
+          },
+        },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = false,
+        },
+        group_empty_dirs = true,
+        show_unloaded = true,
+        window = {
+          mappings = {
+            ['bd'] = 'buffer_delete',
+            ['<bs>'] = 'navigate_up',
+            ['.'] = 'set_root',
+          },
+        },
+      },
+    }
+    vim.cmd [[nnoremap \ :Neotree toggle<CR>]]
+  end,
 }
 
-return M
