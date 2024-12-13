a80989e1508af873259d60a0049dfc159a3c2550
Premnaath <premnaathvaradharajan@gmail.com>
2024-12-13 19:42:26 +0530

    update grug-far to only use default window width

--- nvim/.config/nvim/lua/sudo-prem/plugins/grug-far.lua
+++ nvim/.config/nvim/lua/sudo-prem/plugins/grug-far.lua
@@ -2,7 +2,6 @@ return {
     "MagicDuck/grug-far.nvim",
     config = function()
         require("grug-far").setup({
-            windowCreationCommand = "70 vsplit",
             keymaps = {
                 close = { n = "<localleader>rc" },
                 swapReplacementInterpreter = { n = "<localleader>R" },
