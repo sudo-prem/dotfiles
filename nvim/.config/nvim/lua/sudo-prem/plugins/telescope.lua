a716c44e1bb712e131ceeac74fec2f1660530af7
Premnaath <premnaathvaradharajan@gmail.com>
2025-01-24 21:34:40 +0530

    update telescope keybindings

--- nvim/.config/nvim/lua/sudo-prem/plugins/telescope.lua
+++ nvim/.config/nvim/lua/sudo-prem/plugins/telescope.lua
@@ -64,10 +64,10 @@ return {
         pcall(require("telescope").load_extension, "ui-select")
 
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
+        vim.keymap.set("n", "<leader><leader>", builtin.live_grep, {})
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
         vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
-        vim.keymap.set("n", "<leader>ff", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
         vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
         vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
