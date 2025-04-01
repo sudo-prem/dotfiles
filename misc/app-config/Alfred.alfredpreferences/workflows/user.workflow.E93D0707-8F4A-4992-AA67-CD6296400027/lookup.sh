a8489a2eb446e3cbadc41add27e54970223a32ce
Premnaath <premnaathvaradharajan@gmail.com>
2025-04-01 15:21:05 +0530

    update alfred workflows

--- misc/app-config/Alfred.alfredpreferences/workflows/user.workflow.E93D0707-8F4A-4992-AA67-CD6296400027/lookup.sh
+++ misc/app-config/Alfred.alfredpreferences/workflows/user.workflow.E93D0707-8F4A-4992-AA67-CD6296400027/lookup.sh
@@ -1,43 +1,62 @@
 #!/bin/zsh --no-rcs
 
-readonly whois=$(whois "${1}" | tr -d '\r')
+if [[ "${forceReload}" -ne 1 ]] && [[ -f "${whois_file}" ]] && [[ "$(date -r "${whois_file}" +%s)" -gt "$(date -v -"59"M +%s)" ]]; then
+    # Lookup WHOIS cache file
+    readonly whois=$(cat "${whois_file}")
+else
+    # Lookup WHOIS server
+    [[ "${forceReload}" -eq 1 ]] && 1=$(echo "${1:6}" | head -n 1)
+    readonly whois=$(whois "${1}" | tr -d '\r')
+    mkdir -p "${alfred_workflow_cache}"
+    # Only write to cache if valid entry
+    if [[ -z "$(awk '/(^\% This query returned 0 objects\.|^\% Error: Invalid query)/' <<< "${whois}")" ]]; then
+        echo "${whois}" > "${whois_file}"
+    fi
+fi
+# Skip formatting if viewing as text file
+[[ "${fileView}" -eq 1 ]] && open "${whois_file}" && exit
+
 output="# WHOIS ${1}"
 
 # Domain Information
 if [[ $domainInfo -eq 1 ]]; then
-	emptyCheck=$(awk '/(^Domain Name|^domain)/ && !seen[$0]++ { if ($0 ~ /\./) arr[0] = "* **Domain Name**: " tolower($NF) }
-	/^[Rr]egistrar:[^$]/ && !seen[$0]++ { $1=""; arr[1] = "* **Registrar**:" $0 }
-	/^Creation Date/ && !seen[$0]++ { arr[2] = "* **Creation Date**: " substr($3,0,10) }
-	/Expir.*Date:/ && !seen[$0]++ { arr[3] = "* **Expiry Date**: " substr($NF,0,10) }
-	/(^Updated Date|^[Ll]ast.?[Uu]pdate)/ && !seen[$0]++ { arr[4] = "* **Renewed Date**: " substr($NF,0,10) }
+	emptyCheck=$(awk 'BEGIN { dateRegex = "[0-9]{4}-[0-9]{2}-[0-9]{2}" }
+	/(^[[:blank:]]*Domain Name|^domain)/ && !seen[$0]++ { if ($0 ~ /\./) arr[0] = "* **Domain Name**: " tolower($NF) }
+	/^[[:blank:]]*[Rr]egistrar:[^$]/ && !seen[$0]++ { $1=""; arr[1] = "* **Registrar**:" $0 }
+	/^[[:blank:]]*Creation Date/ && !seen[$0]++ { start = match($0,dateRegex); arr[2] = "* **Creation Date**: " substr($0,start,10) }
+	/Expir.*Date:/ && !seen[$0]++ { start = match($0,dateRegex); arr[3] = "* **Expiry Date**: " substr($0,start,10) }
+	/(^[[:blank:]]*Updated Date|^[Ll]ast.?[Uu]pdate)/ && !seen[$0]++ { start = match($0,dateRegex); arr[4] = "* **Renewed Date**: " substr($0,start,10) }
 	END {
 		for (i = 0; i < length(arr); i++)
 			print arr[i]
 	}' <<< "${whois}")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Domain Information\n${emptyCheck}"
 
-	emptyCheck=$(awk '/^Name Server/ && !seen[tolower($0)]++ { print "* **Name Server**:", $3 }' <<< "${whois}")
+	emptyCheck=$(awk '/^[[:blank:]]*Name Server/ && !seen[tolower($NF)]++ { print "* **Name Server**:", tolower($NF) }' <<< "${whois}")
 	[[ "${emptyCheck}" != "* **Name Server**: " ]] && output+="\n${emptyCheck}"
-	
-	emptyCheck=$(awk '/^Registrar Abuse Contact Email:[^$]/ && !seen[$0]++ { print "* **Registrar Abuse Contact Email**:", tolower($5) }' <<< "${whois}")
-	[[ -n "${emptyCheck}" ]] && output+="\n${emptyCheck}"
+
+	emptyCheck=$(awk '/^[[:blank:]]*Registrar Abuse Contact Email:[^$]/ && !seen[$5]++ { print "* **Registrar Abuse Contact Email**:", tolower($5) }' <<< "${whois}")
+	[[ "${emptyCheck}" != "* **Registrar Abuse Contact Email**: "  ]] && output+="\n${emptyCheck}"
 fi
 
 # Contact Information
+function contactSummary {
+    awk -v pattern="${1}" '$0 ~ pattern && !seen[$0]++ { if (toupper($0) !~ /(REDACTED|GDPR.MASK)/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}"
+}
 if [[ $registrantContact -eq 1 ]]; then
-	emptyCheck=$(awk '/^Registrant/ && !seen[$0]++ { if (toupper($0) !~ /REDACTED/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
+	emptyCheck=$(contactSummary "^Registrant")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Registrant Contact\n${emptyCheck}"
 fi
 if [[ $adminContact -eq 1 ]]; then
-	emptyCheck=$(awk '/^Admin/ && !seen[$0]++ { if (toupper($0) !~ /REDACTED/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
+	emptyCheck=$(contactSummary "^Admin")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Administrative Contact\n${emptyCheck}"
 fi
 if [[ $techContact -eq 1 ]]; then
-	emptyCheck=$(awk '/^Tech/ && !seen[$0]++ { if (toupper($0) !~ /REDACTED/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
+	emptyCheck=$(contactSummary "^Tech")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Technical Contact\n${emptyCheck}"
 fi
 if [[ $billingContact -eq 1 ]]; then
-	emptyCheck=$(awk '/^Billing/ && !seen[$0]++ { if (toupper($0) !~ /REDACTED/ && $0 !~ /(:$|: $)/ && $0 !~ /Email[^@]*$/) print "*", $0 }' <<< "${whois}")
+	emptyCheck=$(contactSummary "^Billing")
 	[[ -n "${emptyCheck}" ]] && output+="\n\n### Billing Contact\n${emptyCheck}"
 fi
 
@@ -47,10 +66,5 @@ output+=$(echo "\n\n## Raw WHOIS Data\n
 ${whois}
 \`\`\`")
 
-# Export to File or Text View
-if [[ "${textView}" -eq 1 ]]; then
-    echo "${output}"
-else
-    mkdir -p ${alfred_workflow_cache}
-    echo "${whois}" > ${alfred_workflow_cache}/whois.txt
-fi
+# Output Formatted WHOIS Data to Text View
+echo "${output}"
---
+++ misc/app-config/Alfred.alfredpreferences/workflows/user.workflow.E93D0707-8F4A-4992-AA67-CD6296400027/search.sh
@@ -0,0 +1,83 @@
+#!/bin/zsh --no-rcs
+
+# Set Loading subtitle
+if [[ "${loading}" -eq 1 && "${autocomplete}" -ne 1 ]]; then
+	sub="Loading..."
+	subCMD="${sub}"
+else
+	sub="Search WHOIS for '${1}'"
+	subCMD="${sub} in text file"
+fi
+
+# Get Last Updated Time
+function getLastUpdated {
+    if [[ ${1} -eq 0 || ${1} -gt 59 ]]; then
+        lastUpdated="Just now"
+    elif [[ ${1} -eq 1 ]]; then
+        lastUpdated="${1} minute ago"
+    else
+        lastUpdated="${1} minutes ago"
+    fi
+}
+whois_file="${alfred_workflow_cache}/${1//\//%2F}.txt"
+[[ -f "${whois_file}" ]] && getLastUpdated "$((($(date +%s)-$(date -r "${whois_file}" +%s))/60))" || lastUpdated="Just now"
+
+# Autocomplete
+arg="${1}"
+function autocomplete {
+    while IFS= read -r file; do
+        fileName=$(basename -s ".txt" "${file}")
+        fileName="${fileName//\%2F/\/}"
+        getLastUpdated "$((($(date +%s)-$(date -r "${file}" +%s))/60))"
+
+        [[ "${fileName}" != "${arg}" ]] && echo '{
+            "title": "'"${fileName}"'",
+            "arg": "'"${fileName}"'",
+            "autocomplete": "'"${fileName}"'",
+            "icon": { "path": "suggestion.png" },
+            "mods": { "cmd": { "variables": { "autocomplete": "1" } } },
+            "variables": {
+                "loading": "1",
+                "lastUpdated": "'"${lastUpdated}"'",
+                "whois_file": "'"${file}"'",
+                '"$([[ ${quickAutocomplete} -ne 1 ]] && echo '"autocomplete": "1"')"'
+            }
+        },'
+    done <<< "${domainList}"
+}
+if [[ ${useAutocomplete} -eq 1 ]]; then
+    domainList=$(find ${alfred_workflow_cache} -maxdepth 1 -iname "${1}*.txt" | head -n 8)
+    topDomain=$(basename -s ".txt" "$(echo ${domainList} | head -n 1)")
+    [[ -n ${topDomain} ]] && autocompleteJSON="${topDomain//\%2F/\/}" || autocompleteJSON="${1}"
+else
+    autocompleteJSON="${1}"
+fi
+
+# JSON Output
+cat << EOB
+{
+"variables": {
+	"autocomplete": "0",
+	"lastUpdated": "${lastUpdated}",
+	"whois_file": "${whois_file}"
+},
+"items": [
+	{
+		"title": "${alfred_workflow_name}",
+		"subtitle": "${sub}",
+		"arg": "${1}",
+		"autocomplete": "${autocompleteJSON}",
+		"variables": { "loading": "1" },
+		"mods": {
+			"cmd": {
+				"subtitle": "${subCMD}",
+				"variables": {
+				    "loading": "1",
+				    "fileView": "1"
+				}
+			}
+		}
+	},
+	$([[ ${useAutocomplete} -eq 1 && ${loading} -ne 1 ]] && autocomplete)
+]}
+EOB
