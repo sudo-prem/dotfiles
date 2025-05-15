d6246adb5cabb48a7fb9a0ccbd622658c214a683
Premnaath <premnaathvaradharajan@gmail.com>
2025-05-15 09:36:43 +0530

    update whoisup alfred workflow

--- misc/app-config/Alfred.alfredpreferences/workflows/user.workflow.E93D0707-8F4A-4992-AA67-CD6296400027/search.sh
+++ misc/app-config/Alfred.alfredpreferences/workflows/user.workflow.E93D0707-8F4A-4992-AA67-CD6296400027/search.sh
@@ -11,12 +11,16 @@ fi
 
 # Get Last Updated Time
 function getLastUpdated {
-    if [[ ${1} -eq 0 || ${1} -gt 59 ]]; then
+    if [[ ${1} -eq 0 || ${1} -gt 359 ]]; then
         lastUpdated="Just now"
     elif [[ ${1} -eq 1 ]]; then
         lastUpdated="${1} minute ago"
-    else
+    elif [[ ${1} -gt 1 && ${1} -lt 60 ]]; then
         lastUpdated="${1} minutes ago"
+    elif [[ ${1} -ge 60 && ${1} -lt 120 ]]; then
+        lastUpdated="$((${1}/60)) hour ago"
+    else
+        lastUpdated="$((${1}/60)) hours ago"
     fi
 }
 whois_file="${alfred_workflow_cache}/${1//\//%2F}.txt"
@@ -29,6 +33,7 @@ function autocomplete {
         fileName=$(basename -s ".txt" "${file}")
         fileName="${fileName//\%2F/\/}"
         getLastUpdated "$((($(date +%s)-$(date -r "${file}" +%s))/60))"
+        # hex code: #4CA9F6
 
         [[ "${fileName}" != "${arg}" ]] && echo '{
             "title": "'"${fileName}"'",
@@ -46,7 +51,11 @@ function autocomplete {
     done <<< "${domainList}"
 }
 if [[ ${useAutocomplete} -eq 1 ]]; then
-    domainList=$(find ${alfred_workflow_cache} -maxdepth 1 -iname "${1}*.txt" | head -n 8)
+    if [[ ${1} != "."* ]]; then
+        domainList=$(find ${alfred_workflow_cache} -maxdepth 1 -iname "${1}*.txt" | head -n 8)
+    else
+        domainList=$(find ${alfred_workflow_cache} -maxdepth 1 -iname "*${1}*.txt" | head -n 8)
+    fi
     topDomain=$(basename -s ".txt" "$(echo ${domainList} | head -n 1)")
     [[ -n ${topDomain} ]] && autocompleteJSON="${topDomain//\%2F/\/}" || autocompleteJSON="${1}"
 else
