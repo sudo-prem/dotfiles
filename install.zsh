416c3fed17b1ea3cca213bf8568a07f2246cb3e6
sudo-prem <premnaathvaradharajan@gmail.com>
2025-05-27 17:14:17 +0530

    update install.zsh to reload shell after brew

--- install.zsh
+++ install.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${(%):-%x}" )" && pwd )
-DOT_FOLDERS="stow,wezterm,bat,colima,fzf,zsh,lsd,git,lazygit,lazydocker,vim,nvim,zed,htop,format"
+DOT_FOLDERS="brew,stow,wezterm,bat,colima,fzf,zsh,lsd,git,lazygit,lazydocker,vim,nvim,zed,htop,format"
 
 for folder in ${(s/,/)DOT_FOLDERS}; do
   echo "[+] Folder :: $folder"
@@ -10,6 +10,12 @@ for folder in ${(s/,/)DOT_FOLDERS}; do
     chmod 777 $folder/install.zsh
     echo "[+] Running install.zsh in $folder"
     source "$folder/install.zsh"
+
+	if [ "$folder" = "brew" ]; then
+      echo "[+] Reloading shell after brew install..."
+      exec $SHELL -l
+    fi
+
   else
     stow --ignore=README.md --ignore=LICENSE \
       -t $HOME -D $folder
