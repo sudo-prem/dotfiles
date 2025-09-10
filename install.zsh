3978fe348c396e4f7e9c4fec06c30352e7240c11
sudo-prem <premnaathvaradharajan@gmail.com>
2025-09-09 18:50:44 -0700

    feat: configure stow for gemini-cli

--- install.zsh
+++ install.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${(%):-%x}" )" && pwd )
-DOT_FOLDERS="brew,stow,wezterm,bat,colima,fzf,zsh,lsd,git,lazygit,lazydocker,vim,nvim,zed,htop,format"
+DOT_FOLDERS="brew,stow,wezterm,bat,colima,fzf,zsh,lsd,git,lazygit,lazydocker,vim,nvim,zed,htop,format,gemini"
 
 for folder in ${(s/,/)DOT_FOLDERS}; do
   echo "[+] Folder :: $folder"
