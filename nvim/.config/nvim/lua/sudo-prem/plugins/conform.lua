34201d5214866363a9d953184dc7ea9305e3cac6
sudo-prem <premnaathvaradharajan@gmail.com>
2025-12-01 06:45:19 -0800

    feat: disable format on save for html

--- nvim/.config/nvim/lua/sudo-prem/plugins/conform.lua
+++ nvim/.config/nvim/lua/sudo-prem/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
-			local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = { c = true, cpp = true, html = true }
 			local lsp_format_opt
 			if disable_filetypes[vim.bo[bufnr].filetype] then
 				lsp_format_opt = "never"
