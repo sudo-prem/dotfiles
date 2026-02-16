a3b175c0f493489fbf6bb49c6e2217b04e705112
sudo-prem <premnaathvaradharajan@gmail.com>
2026-02-15 22:55:03 -0800

    feat: fix brew install.zsh script dir

--- brew/install.zsh
+++ brew/install.zsh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${(%):-%x}" )" && pwd )
+
 if ! command -v brew &> /dev/null; then
     echo "Homebrew is not installed."
     exit 1
 fi
 
-if [ ! -f Brewfile ]; then
+if [ ! -f "$SCRIPT_DIR/Brewfile" ]; then
     echo "Brewfile not found."
     exit 1
 fi
 
-brew bundle install --file=Brewfile
+brew bundle install --file="$SCRIPT_DIR/Brewfile"
