03ddbf39a6b7fbec9773070f4f0b642a6cbe5d43
sudo-prem <premnaathvaradharajan@gmail.com>
2026-06-16 21:02:20 -0700

    chore: update wezterm config to support tmux panes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -22,137 +22,34 @@ config.window_padding = {
 
 config.default_workspace = "sudo-term"
 config.scrollback_lines = 3000
-
+config.send_composed_key_when_left_alt_is_pressed = false
 config.inactive_pane_hsb = {
 	brightness = 0.35,
 }
 
-config.use_fancy_tab_bar = false
-config.status_update_interval = 1000
-config.tab_bar_at_bottom = true
+config.enable_tab_bar = false
 config.native_macos_fullscreen_mode = true
+config.disable_default_key_bindings = true
 
-wezterm.on("update-status", function(window, pane)
-	local stat = window:active_workspace()
-	local stat_color = "#f7768e"
-
-	if window:active_key_table() then
-		stat = window:active_key_table()
-		stat_color = "#7dcfff"
-	end
-	if window:leader_is_active() then
-		stat = "leader"
-		stat_color = "#bb9af7"
-	end
-
-	window:set_left_status(wezterm.format({
-		{ Foreground = { Color = stat_color } },
-		{ Text = "  " },
-		{ Text = wezterm.nerdfonts.md_apple_keyboard_command .. "  " .. stat },
-		{ Text = " |" },
-	}))
-
-	local basename = function(s)
-		return string.gsub(s, "(.*[/\\])(.*)", "%2")
-	end
-
-	local cwd_uri = pane:get_current_working_dir()
-	local cwd = basename(cwd_uri.file_path)
-	local cmd = basename(pane:get_foreground_process_name())
-
-	window:set_right_status(wezterm.format({
-		{ Text = wezterm.nerdfonts.cod_folder_opened .. "  " .. cwd },
-		{ Foreground = { Color = "#e0af68" } },
-		{ Text = " | " },
-		{ Text = wezterm.nerdfonts.oct_code .. "  " .. cmd },
-		"ResetAttributes",
-		{ Text = "  " },
-	}))
-end)
-
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 777 }
 config.keys = {
-	{ key = "c",          mods = "LEADER", action = act.ActivateCopyMode },
-	{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
-
-	{
-		key = 'h',
-		mods = 'ALT',
-		action = act.SplitPane {
-			direction = 'Left',
-			size = { Percent = 50 },
-		},
-	},
-	{
-		key = 'j',
-		mods = 'ALT',
-		action = act.SplitPane {
-			direction = 'Down',
-			size = { Percent = 50 },
-		},
-	},
-	{
-		key = 'k',
-		mods = 'ALT',
-		action = act.SplitPane {
-			direction = 'Up',
-			size = { Percent = 50 },
-		},
-	},
-	{
-		key = 'l',
-		mods = 'ALT',
-		action = act.SplitPane {
-			direction = 'Right',
-			size = { Percent = 50 },
-		},
-	},
-	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
-	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
-	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-
-	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
-
-	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
-	{
-		key = "R",
-		mods = "LEADER",
-		action = act.PromptInputLine({
-			description = wezterm.format({
-				{ Attribute = { Intensity = "Bold" } },
-				{ Foreground = { AnsiColor = "Fuchsia" } },
-				{ Text = "rename tab to" },
-			}),
-			action = wezterm.action_callback(function(window, _, line)
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
-	},
-
-	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
-}
-
-config.key_tables = {
-	resize_pane = {
-		{ key = "h",      action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "j",      action = act.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "k",      action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "l",      action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter",  action = "PopKeyTable" },
-	},
-	move_tab = {
-		{ key = "h",      action = act.MoveTabRelative(-1) },
-		{ key = "j",      action = act.MoveTabRelative(-1) },
-		{ key = "k",      action = act.MoveTabRelative(1) },
-		{ key = "l",      action = act.MoveTabRelative(1) },
-		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter",  action = "PopKeyTable" },
-	},
+	-- clipboard
+	{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
+	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+	-- front size
+	{ key = "=", mods = "SUPER", action = act.IncreaseFontSize },
+	{ key = "-", mods = "SUPER", action = act.DecreaseFontSize },
+	{ key = "0", mods = "SUPER", action = act.ResetFontSize },
+	-- search scrollback
+	{ key = "f", mods = "SUPER", action = act.Search("CurrentSelectionOrEmptyString") },
+	-- fullscreen
+	{ key = "Enter", mods = "ALT", action = act.ToggleFullScreen },
+	-- new OS window
+	{ key = "n", mods = "SUPER", action = act.SpawnWindow },
+	-- tmux passthrough
+	{ key = "t", mods = "SUPER", action = act.SendString("\x01t") },
+	{ key = "w", mods = "SUPER", action = act.SendString("\x01w") },
+	{ key = "Tab", mods = "CTRL", action = act.SendString("\x01\t") },
+	{ key = "Tab", mods = "CTRL|SHIFT", action = act.SendString("\x01\x1b[Z") },
 }
 
 return config
